62df4f42588b71ae26cc0d094806b9256c8bf0d0
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-06-08 17:59:09 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//HERO SLIDER
 $(document).ready(function () {
   $("#hero-slider").owlCarousel({
     loop: true,
@@ -5,12 +6,44 @@ $(document).ready(function () {
     nav: true,
     items: 1,
     smartSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
     navText: ["PREV", "NEXT"],
     dots: false,
     responsive: {
-      0: {},
-      600: {},
+      0: { nav: false },
+      768: { nav: true },
       1000: {},
     },
+  });
+});
+
+//PROJECT SLIDER
+$(document).ready(function () {
+  $("#project-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    smartSpeed: 1000,
+    dots: false,
+    margin: 24,
+    navText: ["PREV", "NEXT"],
+    responsive: {
+      0: { items: 1, nav: false, margin: 0 },
+      768: { items: 2 },
+      1140: { items: 2, center: true, dots: true },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $("#reviews-slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 1,
+    smartSpeed: 800,
   });
 });
